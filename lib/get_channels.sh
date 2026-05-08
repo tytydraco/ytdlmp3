7b3a927aaaad7491d4b8c82735d3357cef4d745f
Tyler Nijmeh <tylernij@gmail.com>
2026-05-08 08:05:39 -0500

    Remove unused files
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/get_channels.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function get_channels() {
-    yt-dlp \
-        --cookies-from-browser firefox \
-        --flat-playlist \
-        --print "%(channel)s | https://www.youtube.com/channel/%(channel_id)s" \
-        "https://www.youtube.com/feed/channels" | awk -F " | " '{print (NF>1)? $NF : ""}'
-}
