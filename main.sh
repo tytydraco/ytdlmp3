8afd3e20bb761cd138aeb323a58fe33f8052b628
Tyler Nijmeh <tylernij@gmail.com>
2026-06-02 08:58:49 -0500

    Fix exports missing
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.sh
+++ main.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 MP3C_DIR="${SCRIPT_DIR}/mp3c"
 CONFIG_FILE="$SCRIPT_DIR/config.sh"
 
+FFMPEG_YP3="$MP3C_DIR/tools/ffmpeg/ffmpeg-yp3-patch/static/ffmpeg"
+ATJ_AVI_ENCODER="$MP3C_DIR/tools/ffmpeg/atj-avi-encoder/make-atj-avi-encoder.sh"
+
 function load_mp3c_converters() {
     while IFS= read -r -d '' src; do
         source "$src"
@@ -19,8 +22,7 @@ function main() {
     fi
 
     # Pull latest submodules.
-    git submodule init
-    git submodule update --remote
+    git submodule update --init --recursive --remote
 
     source "$CONFIG_FILE"
     load_mp3c_converters
@@ -44,6 +46,8 @@ function main() {
 export SCRIPT_DIR
 export MP3C_DIR
 export CONFIG_FILE
+export FFMPEG_YP3
+export ATJ_AVI_ENCODER
 
 # Execute the program.
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main
